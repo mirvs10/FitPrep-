c01bcb4347b3abe934fc34a378163a3493880bcf
mirvs10 <cafter68@gmail.com>
2026-07-19 20:45:34 -0500

    Correcion vite

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import { TanStackStartVite } from "@tanstack/react-start/plugin/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
-    TanStackStartVite({ server: { entry: "server" } }),
+    tanstackStart({ server: { entry: "server" } }),
     viteReact(),
     tailwindcss()
   ],
