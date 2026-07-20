8acb72d0f3fe8fb2314bf025b4a865b3a7a0d63f
mirvs10 <cafter68@gmail.com>
2026-07-19 20:43:39 -0500

    Correcion vite con Vercel

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import { TanStackStartVite } from "@tanstack/react-start/plugin";
+import { TanStackStartVite } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
